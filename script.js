cae8c4eb4e00f916bbbd493430695b4fb83c8eb4
Dylan Stratton <dm.stratton94@gmail.com>
2017-02-01 17:39:53 -0600

    added digital art section

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
+var canvas;
+var img;
+var point;
+
+function preload(){
+  img = loadImage(img/about-opt.jpg);
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(img.width, img.height);
+  canvas.parent('about-point');
+  
+  point = 10;
+  imageMode(CENTER);
+  noStroke();
+  background(255);
+  img.loadPixels();
 }
 
 function draw() {
-  
+  var x = floor(random(img.width));
+  var y = floor(random(img.height));
+  var pix = img.get(x,y);
+  fill(pix, 128);
+  ellipse(x, y, point, point);
 }
 
-function mouseDragged(){
-  noStroke();
-  fill(125, 230, 160, 50);
-  ellipse(mouseX,mouseY,20,20);
-}
-
-function windowResized(){
-  resizeCanvas = (windowWidth, windowHeight);
-}
+//function mouseDragged(){
+//  noStroke();
+//  fill(125, 190, 160, 10);
+//  ellipse(mouseX,mouseY,20,20);
+//}
+//
+//function windowResized(){
+//  resizeCanvas = (windowWidth, windowHeight);
+//}
 
 //function clear(){
 //  if (key == 'c'){
